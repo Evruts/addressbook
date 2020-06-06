cc8db42800efc11049a98a2be6f6cef1ad82a285
Paul Axinte <po.axinte@gmail.com>
2020-06-06 18:42:11 +0300

    Add tasks and snippets

--- db.sql
+++ db.sql
@@ -1,6 +1,23 @@
 # Ca sa rulezi un statement de SQL, click pe el si CTRL + Enter
+#
+# Credentialele:
+# host: localhost
+# user: root
+# pass:     (empty)
+#
+
 CREATE DATABASE contacte;
 
-# 1. Creeaza tabel cu contacte
+# 1. Creeaza tabel cu contacte: nume, prenume, oras, nr_telefon, varsta
 # 2. Insereaza 5-10 contacte in tabel;
+
+
+# Exercitii:
+#    I: Scrie query care selecteaza :
+#      a. contactele din iasi
+#      b. contactele cu varsta mai mare de 18 ani, ordonate descrescator
+#      c. contactele care au nume ce incep cu litera A
+#    II:  Query care sterge:
+#       a. contactele din Vaslui
+#    III: Query care updateaza contactele din iasi sa aiba toti 25 de ani
 #
--- laborator1.php
+++ laborator1.php
@@ -2,7 +2,10 @@
 0. conexiune baza de date in PHP
 1. arata elementele din baza de date pe pagina
  -->
+<?php
+// Aici scrie codul ce conecteaza la baza de date;
 
+?>
 <html>
 <head>
     <title>Title of the document</title>
@@ -12,6 +15,8 @@
 <h1>This is a heading</h1>
 <p>This is a <br> paragraph.</p>
 <?php echo "Asd"; ?>
+
+
 <img src="workout.jpg" alt="workout" class="center" >
 
 </body>
