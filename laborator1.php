dde83f82a1d008651fc2ade48a07450607d9dda2
Paul Axinte <po.axinte@gmail.com>
2020-06-06 18:20:22 +0300

    Initial commit

---
+++ laborator1.php
@@ -0,0 +1,15 @@
+
+
+<html>
+<head>
+    <title>Title of the document</title>
+</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<h1>This is a heading</h1>
+<p>This is a <br> paragraph.</p>
+<?php echo "Asd"; ?>
+<img src="workout.jpg" alt="workout" class="center" >
+
+</body>
+</html>
